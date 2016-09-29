705599c7f7fc4fb42fe5b9d3e28b42f7545e9176
root <root@localhost.localdomain>
2016-09-29 16:32:30 -0400

    working correctly

--- scanner.py
+++ scanner.py
@@ -4,7 +4,7 @@ import sys, time
 from datetime import datetime
 
 host = ''
-max_port = 5000
+max_port = 50000
 min_port = 1
 
 
@@ -12,13 +12,13 @@ def scan_host(host, port, r_code=1):
     try:
         s = socket(AF_INET, SOCK_STREAM)  # set up a socket
 
-        code = s.connect_ex(host, port)  # connect to target using socket
+        code = s.connect_ex((host, port))  # connect to target using socket
 
         if code == 0:  # if the connection attempt is successful...
             r_code = code  # log result then close the connection
             s.close()
     except Exception, e:
-        pass
+        print(e)
 
     return r_code  # return the connection result
 
@@ -29,15 +29,21 @@ except KeyboardInterrupt:
     print("[*] Application shutting down")
     sys.exit(1)
 
-host_ip = gethostbyname(host)  # returns the hosts ip address based on a URL
+#host_ip = gethostbyname(host)  # returns the hosts ip address based on a URL
+#print("Host: " + host_ip)
 
 for port in range(min_port, max_port):  # for each of the ports in the range do...
+    #print(port)
     try:
         response = scan_host(host, port)  # scan
-
         if response == 0:  # if response is 0... port is open
             print("[*] Port %d: open" % (port))
     except Exception, e:
-        pass
-
+	print(e)
+    except KeyboardInterrupt:
+        print("\n\n[*] User requested interrupt.")
+        print("[*] Application shutting down")
+        sys.exit(1) 
+        
+print("Complete")
 
